21252c99c6806b637aece9b36e7886ff08a97a4c
BhaskarDutta2209 <bhaskardutta2209@gmail.com>
2019-10-04 20:28:14 +0530

    Devided the code into various files for eassy understanding and removed certain non important lines

--- dodgeGame.py
+++ dodgeGame.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import random
+from settings import *
 
 pygame.mixer.pre_init(44100, 16, 2, 4096)
 pygame.init()
@@ -8,27 +9,11 @@ pygame.init()
 #############
 crash_sound = pygame.mixer.Sound("doublebass.wav")
 #############
- 
-display_width = 800
-display_height = 600
- 
-black = (0,0,0)
-white = (255,255,255)
 
-red = (200,0,0)
-green = (0,200,0)
-
-bright_red = (255,0,0)
-bright_green = (0,255,0)
- 
-block_color = (53,115,255)
- 
-car_width = 73
- 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('A bit Racey')
 clock = pygame.time.Clock()
- 
+
 carImg = pygame.image.load('racecar.png')
 gameIcon = pygame.image.load('logo.png')
 
@@ -36,23 +21,23 @@ pygame.display.set_icon(gameIcon)
 
 pause = False
 #crash = True
- 
+
 def things_dodged(count):
     font = pygame.font.SysFont("comicsansms", 25)
     text = font.render("Dodged: "+str(count), True, black)
     gameDisplay.blit(text,(0,0))
- 
+
 def things(thingx, thingy, thingw, thingh, color):
     pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
- 
+
 def car(x,y):
     gameDisplay.blit(carImg,(x,y))
- 
+
 def text_objects(text, font):
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
- 
- 
+
+
 def crash():
     ####################################
     pygame.mixer.Sound.play(crash_sound)
@@ -62,21 +47,21 @@ def crash():
     TextSurf, TextRect = text_objects("You Crashed", largeText)
     TextRect.center = ((display_width/2),(display_height/2))
     gameDisplay.blit(TextSurf, TextRect)
-    
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-                
-        
+
+
 
         button("Play Again",150,450,100,50,green,bright_green,game_loop)
         button("Quit",550,450,100,50,red,bright_red,quitgame)
 
         pygame.display.update()
-        clock.tick(15) 
+        clock.tick(15)
 
 def button(msg,x,y,w,h,ic,ac,action=None):
     mouse = pygame.mouse.get_pos()
@@ -85,15 +70,15 @@ def button(msg,x,y,w,h,ic,ac,action=None):
     if x+w > mouse[0] > x and y+h > mouse[1] > y:
         pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
         if click[0] == 1 and action != None:
-            action()         
+            action()
     else:
         pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
     smallText = pygame.font.SysFont("comicsansms",20)
     textSurf, textRect = text_objects(msg, smallText)
     textRect.center = ( (x+(w/2)), (y+(h/2)) )
     gameDisplay.blit(textSurf, textRect)
-    
- 
+
+
 def quitgame():
     pygame.quit()
     quit()
@@ -102,7 +87,7 @@ def unpause():
     global pause
     pygame.mixer.music.unpause()
     pause = False
-    
+
 
 def paused():
     ############
@@ -112,7 +97,7 @@ def paused():
     TextSurf, TextRect = text_objects("Paused", largeText)
     TextRect.center = ((display_width/2),(display_height/2))
     gameDisplay.blit(TextSurf, TextRect)
-    
+
 
     while pause:
         for event in pygame.event.get():
@@ -125,7 +110,7 @@ def paused():
         button("Quit",550,450,100,50,red,bright_red,quitgame)
 
         pygame.display.update()
-        clock.tick(15)   
+        clock.tick(15)
 
 
 def game_intro():
@@ -138,7 +123,7 @@ def game_intro():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-                
+
         gameDisplay.fill(white)
         largeText = pygame.font.SysFont("comicsansms",115)
         TextSurf, TextRect = text_objects("A bit Racey", largeText)
@@ -150,12 +135,12 @@ def game_intro():
 
         pygame.display.update()
         clock.tick(15)
-        
-        
-    
-    
 
-    
+
+
+
+
+
 def game_loop():
     global pause
     ############
@@ -164,28 +149,28 @@ def game_loop():
     ############
     x = (display_width * 0.45)
     y = (display_height * 0.8)
- 
+
     x_change = 0
- 
+
     thing_startx = random.randrange(0, display_width)
     thing_starty = -600
     thing_speed = 4
     thing_width = 100
     thing_height = 100
- 
+
     thingCount = 1
- 
+
     dodged = 0
- 
+
     gameExit = False
- 
+
     while not gameExit:
- 
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
- 
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     x_change = -5
@@ -194,40 +179,38 @@ def game_loop():
                 if event.key == pygame.K_p:
                     pause = True
                     paused()
-                    
- 
+
+
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
- 
+
         x += x_change
         gameDisplay.fill(white)
- 
+
         things(thing_startx, thing_starty, thing_width, thing_height, block_color)
- 
- 
-        
+
+
+
         thing_starty += thing_speed
         car(x,y)
         things_dodged(dodged)
- 
+
         if x > display_width - car_width or x < 0:
             crash()
- 
+
         if thing_starty > display_height:
             thing_starty = 0 - thing_height
             thing_startx = random.randrange(0,display_width)
             dodged += 1
             thing_speed += 1
             thing_width += (dodged * 1.2)
- 
+
         if y < thing_starty+thing_height:
-            print('y crossover')
- 
+
             if x > thing_startx and x < thing_startx + thing_width or x+car_width > thing_startx and x + car_width < thing_startx+thing_width:
-                print('x crossover')
                 crash()
-        
+
         pygame.display.update()
         clock.tick(60)
 
---
+++ settings.py
@@ -0,0 +1,17 @@
+# This contains all the defined constants
+
+display_width = 800
+display_height = 600
+
+black = (0,0,0)
+white = (255,255,255)
+
+red = (200,0,0)
+green = (0,200,0)
+
+bright_red = (255,0,0)
+bright_green = (0,255,0)
+
+block_color = (53,115,255)
+
+car_width = 73
